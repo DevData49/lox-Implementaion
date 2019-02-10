7ce1b3d7a841cdc3624964e78a4e1c6167e97e5a
Dev Data <debadattahembram@gmail.com>
2019-02-10 21:57:02 +0530

    Created the interpreter class with expression evaluating capabilities

--- java/com/devdata/lox/Lox.java
+++ java/com/devdata/lox/Lox.java
@@ -8,68 +8,75 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Lox{
-  static boolean hadError = false;
+public class Lox {
+      private static final Interpreter interpreter = new Interpreter();
+      static boolean hadError = false;
+      static boolean hadRuntimeError = false;
 
-  public static void main(String[] args) throws IOException{
-    if(args.length > 1){
-      System.out.println("usage: jLox [script]");
-      System.exit(64);
-    }else if(args.length == 1){
-      runFile(args[0]);
-    }else{
-      runPrompt();
+      public static void main(String[] args) throws IOException{
+        if(args.length > 1){
+          System.out.println("usage: jLox [script]");
+          System.exit(64);
+        }else if(args.length == 1){
+          runFile(args[0]);
+        }else{
+          runPrompt();
+        }
+      }
+
+      //Handle the execution of a program from a file when its path is provided in
+      //commandline.
+      private static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes,Charset.defaultCharset()));
+        if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
+      }
+
+      //starts an interactive repel if no path to a script is provided
+      private static void runPrompt() throws IOException{
+        InputStreamReader input =  new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+          System.out.print("> ");
+          run(reader.readLine());
+          hadError = false;
+        }
+      }
+
+    //core function which handles actual interpreation and execution of source code
+    private static void run(String source){
+      Scanner scanner = new Scanner(source);
+      List<Token> tokens = scanner.scanTokens();
+      Parser parser = new Parser(tokens);
+      Expr expression = parser.parse();
+
+      if(hadError) return;
+
+      interpreter.interpret(expression);
     }
-  }
 
-  //Handle the execution of a program from a file when its path is provided in
-  //commandline.
-  private static void runFile(String path) throws IOException{
-    byte[] bytes = Files.readAllBytes(Paths.get(path));
-    run(new String(bytes,Charset.defaultCharset()));
-    if(hadError)
-    System.exit(65);
-  }
-
-  //starts an interactive repel if no path to a script is provided
-  private static void runPrompt() throws IOException{
-    InputStreamReader input =  new InputStreamReader(System.in);
-    BufferedReader reader = new BufferedReader(input);
-
-    for(;;){
-      System.out.print("> ");
-      run(reader.readLine());
-      hadError = false;
+    //error-reporting methiod
+    static void error(int line, String message){
+      report(line,"",message);
     }
-  }
 
-//core function which handles actual interpreation and execution of source code
-private static void run(String source){
-  Scanner scanner = new Scanner(source);
-  List<Token> tokens = scanner.scanTokens();
-  Parser parser = new Parser(tokens);
-  Expr expression = parser.parse();
+    private static void report(int line, String where, String message){
+      System.err.println("[line "+ line + "] Error " +where+ ": "+message);
+      hadError = true;
+    }
 
-  if(hadError) return;
-  
-  System.out.println(new AstPrinter().print(expression));
-}
+    static void error(Token token, String message){
+      if(token.type == TokenType.EOF){
+        report(token.line, "at end", message);
+      } else {
+        report(token.line, " at ''"+token.lexeme+"' ", message);
+      }
+    }
 
-//error-reporting methiod
-static void error(int line, String message){
-  report(line,"",message);
-}
-
-private static void report(int line, String where, String message){
-  System.err.println("[line "+ line + "] Error " +where+ ": "+message);
-  hadError = true;
-}
-
-static void error(Token token, String message){
-  if(token.type == TokenType.EOF){
-    report(token.line, "at end", message);
-  } else {
-    report(token.line, " at ''"+token.lexeme+"' ", message);
-  }
-}
+    static void runtimeError(RuntimeError error){
+      System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+      hadRuntimeError = true;
+    }
 }
