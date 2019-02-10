a9e95a8c44df2a67449c0855dae8aaa9446f2233
Dev Data <debadattahembram@gmail.com>
2019-02-10 17:02:55 +0530

    Created the parcer class which can now build out AST

--- java/com/devdata/lox/Lox.java
+++ java/com/devdata/lox/Lox.java
@@ -47,10 +47,12 @@ public class Lox{
 private static void run(String source){
   Scanner scanner = new Scanner(source);
   List<Token> tokens = scanner.scanTokens();
+  Parser parser = new Parser(tokens);
+  Expr expression = parser.parse();
 
-  for(Token token:tokens){
-    System.out.println(token);
-  }
+  if(hadError) return;
+  
+  System.out.println(new AstPrinter().print(expression));
 }
 
 //error-reporting methiod
@@ -63,4 +65,11 @@ private static void report(int line, String where, String message){
   hadError = true;
 }
 
+static void error(Token token, String message){
+  if(token.type == TokenType.EOF){
+    report(token.line, "at end", message);
+  } else {
+    report(token.line, " at ''"+token.lexeme+"' ", message);
+  }
+}
 }
