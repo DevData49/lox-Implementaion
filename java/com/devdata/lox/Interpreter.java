4e60e255d4753a89897013e71ccfca0161c520b1
Dev Data <debadattahembram@gmail.com>
2019-02-11 19:04:28 +0530

    The interpreter now executes statements

--- java/com/devdata/lox/Interpreter.java
+++ java/com/devdata/lox/Interpreter.java
@@ -1,16 +1,33 @@
 package com.devdata.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
 
-  void interpret(Expr expression){
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+  void interpret(List<Stmt> statements){
     try{
-      Object value = evaluate(expression);
-      System.out.println(stringify(value));
+      for(Stmt statement:statements){
+        execute(statement);
+      }
     }
     catch (RuntimeError error){
       Lox.runtimeError(error);
     }
   }
+  //Statement visitor
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt){
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt){
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
+  }
+  //Expression Visitor
   @Override
   public Object visitLiteralExpr(Expr.Literal expr){
     return expr.value;
@@ -107,6 +124,9 @@ class Interpreter implements Expr.Visitor<Object> {
   //helper
   private Object evaluate(Expr expr){
     return expr.accept(this);
+  }
+  private void execute(Stmt stmt){
+    stmt.accept(this);
   }
 
   private String stringify(Object object){
