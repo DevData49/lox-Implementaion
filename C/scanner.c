a587ad7920485627cd9aa8111554811d0d397b9f
Dev <debadattahembram@gmail.com>
2019-02-23 10:10:30 +0530

    bug fix

--- C/scanner.c
+++ C/scanner.c
@@ -16,6 +16,28 @@ static bool isAtEnd() {
   return *scanner.current == '\0';
 }
 
+static char advance(){
+  scanner.current++;
+  return scanner.current[-1];
+}
+
+static char peek() {
+  return *scanner.current;
+}
+
+static char peekNext(){
+  if(isAtEnd()) return '\0';
+  return scanner.current[1];
+}
+static bool match(char expected){
+  if(isAtEnd()) return false;
+  if(*scanner.current == expected){
+    scanner.current++;
+    return true;
+  }
+  return false;
+}
+
 static Token makeToken(TokenType type){
   Token token;
   token.type = type;
@@ -36,6 +58,77 @@ static Token errorToken(const char* message) {
   return token;
 }
 
+static void skipWhitespace(){
+  for(;;){
+    char c = peek();
+    switch (c):{
+      case ' ':
+      case '\r':
+      case '\t':
+        advance();
+        break;
+
+      case '\n':
+        scanner.line++;
+        advance();
+        break;
+
+      case '/':
+        if(peekNext() == '/') {
+          while(peek() != '\n' && !isAtEnd()) advance();
+        }else{
+          return;
+        }
+        break;
+
+      default:
+        return;
+    }
+  }
+}
+
+static TokenType identifierType(){
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier(){
+  while(isAlpha(peek()) || isDigit(peek())) advance();
+
+  return makeToken(identifierType());
+}
+
+static Token number(){
+  while(isDigit(peek())) advance();
+
+  if(peek() == '.' && isDigit(peekNext())) {
+    advance(); //consume the dot
+
+    while(isDigit(peek())) advance();
+  }
+
+  return makeToken(TOKEN_NUMBER);
+}
+
+static Token string(){
+  while(peek()!='"' && !isAtEnd()){
+    peek() == '\n' scanner.line++;
+    advance();
+  }
+
+  if(isAtEnd()) return errorToken("Unterminated String");
+  advance();
+  return makeToken(TOKEN_STRING);
+}
+
+static bool isDigit(char c){
+  return c>='0' && c <= '9';
+}
+
+static isAlpha(char c){
+  return (c >= 'a' && c <= 'z') ||
+         (c >= 'A' && c <= 'Z') ||
+         (c == '_');
+}
 void initScanner(const char* source) {
   scanner.start = source;
   scanner.current = source;
@@ -43,8 +136,41 @@ void initScanner(const char* source) {
 }
 
 Token scanToken() {
+
+  skipWhitespace();
   scanner.start = scanner.current;
   if(isAtEnd()) return makeToken(TOKEN_EOF);
+
+
+  char c =  advance()
+  if(isAlpha(c)) return identifier();
+  if(isDigit(c)) return number();
+
+  switch (c) {
+    //single character
+    case '(': return makeToken(TOKEN_LEFT_PAREN);
+    case ')': return makeToken(TOKEN_RIGHT_PAREN);
+    case '{': return makeToken(TOKEN_LEFT_BRACE);
+    case '}': return makeToken(TOKEN_RIGHT_BRACE);
+    case ';': return makeToken(TOKEN_SEMICOLON);
+    case ',': return makeToken(TOKEN_COMMA);
+    case '.': return makeToken(TOKEN_DOT);
+    case '-': return makeToken(TOKEN_MINUS);
+    case '+': return makeToken(TOKEN_PLUS);
+    case '/': return makeToken(TOKEN_SLASH);
+    case '*': return makeToken(TOKEN_STAR);
+    //single or double character strings
+    case '!':
+          return makeToken(match('=')? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '=':
+          return makeToken(match('=')? TOKEN_EQUAL_EQUAL: TOKEN_EQUAL);
+    case '<':
+          return makeToken(match('=')? TOKEN_LESS_EQUAL: TOKEN_LESS);
+    case '>':
+          return makeToken(match('=')? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    //Literal tokens
+    case '"': return string();
+  }
 
   return errorToken("Unexpected character.");
 }
