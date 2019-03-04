9082ef3eac692ee8a852f3a72279a8bdc444e0e9
Dev <debadattahembram@gmail.com>
2019-03-04 21:21:22 +0530

    fixed Binary operations to work with the new Value type

--- C/value.h
+++ C/value.h
@@ -10,7 +10,7 @@ typedef enum{
 } ValueType;
 
 typedef struct{
-  ValueType;
+  ValueType type;
   union {
     bool boolean;
     double number;
@@ -19,16 +19,16 @@ typedef struct{
 
 #define IS_BOOL(value)   ((value).type == VAL_BOOL)
 #define IS_NIL(value)    ((value).type == VAL_NIL)
-#define IS_NUMBER(Value) ((value).type == VAL_NUMBER)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 
 #define AS_BOOL(value)   ((value).as.boolean)
-#define AS_NUMBER(Value) ((value).as.number)
+#define AS_NUMBER(value) ((value).as.number)
 
-#define BOOL_VAL(Value) ((Value){VAL_BOOL, {.boolean = value} })
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value} })
 #define NIL_VAL         ((Value){VAL_NIL,{ .number = 0} })
-#define NUMBER_VAL(Value) ((Value){ VAL_NUMBER, { .number = value } })
+#define NUMBER_VAL(value) ((Value){ VAL_NUMBER, { .number = value } })
 
-typedef double Value;
+
 
 typedef struct {
   int capacity;
