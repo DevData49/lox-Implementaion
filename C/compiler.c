0d9100de177aca89248131ca5e73e8809f0fae53
Dev <debadattahembram@gmail.com>
2019-03-09 06:42:15 +0530

    added global varibale definition

--- C/compiler.c
+++ C/compiler.c
@@ -274,12 +274,39 @@ static void parsePrecedence(Precedence precedence){
   }
 }
 
+static uint8_t identifierConstant(Token* name){
+  return makeConstant(OBJ_VAL(copyString(name->start,name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage){
+  consume(TOKEN_IDENTIFIER,errorMessage);
+  return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global){
+  emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+
 static ParseRule* getRule(TokenType type){
   return &rules[type];
 }
 
 void expression(){
   parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void varDeclaration() {
+  uint8_t global = parseVariable("Expect variable name.");
+
+  if(match(TOKEN_EQUAL)){
+    expression();
+  } else {
+    emitByte(OP_NIL);
+  }
+  consume(TOKEN_SEMICOLON,"Expect ';' after variable declaration.");
+
+  defineVariable(global);
 }
 
 static void expressionStatment(){
@@ -316,7 +343,12 @@ static void synchronize(){
 }
 
 static void declaration(){
-  statement();
+  if(match(TOKEN_VAR)){
+    varDeclaration();
+  } else {
+    statement();
+  }
+
   if(parser.panicMode) synchronize();
 }
 
@@ -337,8 +369,6 @@ bool compile(const char* source, Chunk* chunk){
   parser.panicMode = false;
 
   advance();
-  // expression();
-  // consume(TOKEN_EOF,"Expect end of expression.");
   while (!match(TOKEN_EOF)) {
     declaration();
   }
